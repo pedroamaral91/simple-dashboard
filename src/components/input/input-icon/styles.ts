3ff77fc9733484049ef012c98550ed38a457dd54
Douglas Cavalcante <38563983+douglas-cavalcante@users.noreply.github.com>
2020-04-15 00:44:18 -0300

    Frontend 24 (#7)
    * [Frontend-3] - Adiciona componente de Input (#4)
    
    * [Frontend-3] - Adiciona componente de Input
    
    * [Frontend-3] - Aplica correções de exportação e estilo dos Inputs
    
    * [Frontend-5] - Adiciona componente DatePicker
    
    * [frontend-24] - aplica correçoes solicitadas

---
+++ src/components/input/input-icon/styles.ts
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+import icon from '../../../assets/error.svg';
+
+/* Interfaces */
+interface InputProps {
+  error: string;
+}
+
+/* Components */
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 5px;
+`;
+
+export const Label = styled.label`
+  display: inline-block;
+  margin-bottom: 0.5rem;
+  color: ${props => props.theme.text.colors.dark};
+`;
+
+export const WrapperInput = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const IconWrapper = styled.div`
+  position: absolute;
+  top: 22%;
+  left: 0.2rem;
+  color: ${({ theme }) => theme.semantic.secondary};
+`;
+
+export const Input = styled.input<InputProps>`
+  display: block;
+
+  padding: 0.375rem 1.2rem;
+  padding-right: calc(1.5em + 0.75rem);
+
+  /* background styles */
+  background-color: ${props => props.theme.semantic.light};
+  background-clip: padding-box;
+
+  background-image: ${props => (props.error ? `url(${icon})` : 'none')};
+  background-repeat: no-repeat;
+  background-position: right calc(0.375em + 0.1875rem) center;
+  background-size: calc(0.75em + 0.375rem);
+  background-clip: padding-box;
+
+  /* font styles */
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: ${props => props.theme.text.colors.dark};
+
+  border: ${props =>
+    props.error
+      ? `1px solid ${props.theme.semantic.danger}`
+      : `1px solid ${props.theme.semantic.secondary}`};
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  &:focus {
+    outline: 0;
+    box-shadow: ${props =>
+      props.error ? props.theme.focus.danger : props.theme.focus.primary};
+    border: ${props =>
+      props.error
+        ? `1px solid ${props.theme.semantic.danger}}`
+        : `1px solid ${props.theme.semantic.primary}}`};
+  }
+
+  &::placeholder {
+    opacity: 1;
+  }
+`;
+
+export const InvalidFeedback = styled.span`
+  width: 100%;
+  font-size: 80%;
+  color: ${({ theme }) => theme.text.colors.danger};
+  text-align: right;
+`;
