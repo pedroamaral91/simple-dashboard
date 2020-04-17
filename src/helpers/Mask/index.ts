356ca48f756b0e8df96e20f00ab0bb941c8e50f5
Douglas Cavalcante <henriquedouglas2803@gmail.com>
2020-04-17 17:21:58 -0300

    [frontend-28] - Adiciona componente InputMask

---
+++ src/helpers/Mask/index.ts
@@ -0,0 +1,20 @@
+import IMask from 'imask';
+
+export type MaskType = 'Phone' | 'DDD' | 'CPF' | 'CNPJ';
+
+const PATTERNS_MASK = new Map<string, string | RegExp>([
+  ['DDD', '(00)'],
+  ['Phone', '00000-0000'],
+  ['CPF', '000.000.000-00'],
+  ['CNPJ', '00.000.000/0000-00'],
+]);
+
+export function handleMaskedValue(maskType: MaskType, value: string | number) {
+  const config: any = {
+    mask: PATTERNS_MASK.get(maskType),
+  };
+
+  const handleSetMask = IMask.createPipe(config);
+
+  return handleSetMask(value);
+}
