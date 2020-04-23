ff3d37df2fe189a4262bddf7832326699a8abf2e
Douglas Cavalcante <henriquedouglas2803@gmail.com>
2020-04-23 15:56:31 -0300

    Aplica correçoes

--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -1,3 +1,4 @@
 export { default as InputText } from './input-text';
 export { default as InputIcon } from './input-icon';
 export { default as InputMask } from './input-mask';
+export { default as InputMoney } from './input-money';
