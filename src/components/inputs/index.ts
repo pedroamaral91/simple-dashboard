a96056d58d0b203dd2915d9b1b97fbb4b278e82e
Douglas Cavalcante <henriquedouglas2803@gmail.com>
2020-04-23 18:08:20 -0300

    [Frontend-23] - Adiciona componente InputCheckbox

--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -2,3 +2,4 @@ export { default as InputText } from './input-text';
 export { default as InputIcon } from './input-icon';
 export { default as InputMask } from './input-mask';
 export { default as InputMoney } from './input-money';
+export { default as InputCheckbox } from './input-checkbox';
